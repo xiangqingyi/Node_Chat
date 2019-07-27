8ac8ec588ace1fb67ab1a20b3fe484b8957da93b
xiangqingyi <1352493573@qq.com>
2019-07-27 15:10:55 +0800

    [ADD] 增加socket.io 功能

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ let mongoose = require('mongoose');
 let moment = require('moment');
 let _ = require('lodash');
 let session = require('express-session');
+let UserModel = require('./models/user');
+let MessageModel = require('./models/message');
+let http = require('http').Server(app);
 let core = require('./libs/core');
+// let req = require('request');
+app.set('port', process.env.PORT || config.port || 4000);
+let server = app.listen(app.get('port'), function(req, res) {
+  core.logger.info('网站启动端口： '+ server.address().port);
+  core.logger.info('环境变量： '+ config.env);
+  core.logger.info('mongodb url' + config.mongodb.uri);
+  core.logger.info('redis url' + config.redis.host+ ":" + config.redis.port);
+})
+let io = require('socket.io')(server);
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
@@ -54,6 +67,115 @@ app.locals = {
 }
 app.set('config', config);
 
+
+let users = [];
+let usersInfo = [];
+// 每个人连接用户都有专有的socket
+io.on('connection', (socket) => {
+  // 在线人员
+  console.log('connect success');
+  io.emit('displayUser', usersInfo);
+  // 登录验证
+  socket.on('login', async (user) => {
+    let loginUser = await UserModel.findOne({name: user.name});
+    if (!loginUser) {
+      // 该用户不存在
+      socket.emit('nouser');
+    } else if (users.indexOf(user.name) > -1) {
+      socket.emit('loginError'); // 该用户已登录 不能把别人挤下去（暂时）
+    } else if (!user.name || !user.password) {
+      core.logger.info('用户名或者密码不能为空');
+      socket.emit('parameterError');
+    } else if (loginUser.authenticate(user.password)) {
+      // 用户验证成功、
+      users.push(user.name);
+      usersInfo.push(user);
+      
+      socket.emit('loginSuccess');
+      socket.nickname = user.name;
+      // 只能进默认的群组（暂时），应该是可以选择群组进去
+      io.emit('system', {
+        name: user.name,
+        status: '进入'
+      })
+      io.emit('displayUser',usersInfo);
+      await UserModel.update({_id: loginUser._id}, {$set: {
+        online: true
+      }})
+      core.logger.info(users.length + 'user connnect.');
+    } else {
+      // 验证失败
+      io.emit('authenticationError');
+
+    }
+  });
+  // 发送抖动窗口
+  socket.on('shake', () => {
+    socket.emit('shake', {
+      name: '您'
+    });
+    socket.broadcast.emit('shake', {
+      name: socket.nickname
+    });
+  })
+  // 发送消息事件
+  socket.on('sendMsg', async (data) => {
+    let img = '';
+    for (let i = 0; i < usersInfo.length; i++) {
+      if (usersInfo[i].name == socket.nickname) {
+        img = usersInfo[i].img;
+      }
+    }
+    socket.broadcast.emit('receiveMsg', {
+      name: socket.nickname,
+      img: img,
+      msg: data.msg,
+      color: data.color,
+      type: data.type,
+      side: 'left'
+    });
+    socket.emit('receiveMsg', {
+      name: socket.nickname,
+      img: img,
+      msg: data.msg,
+      color: data.color,
+      type: data.type,
+      side: 'right'
+    })
+    let _user = await UserModel.findOne({name: socket.nickname});
+    let newMsg = {
+      author: _user._id,
+      content: data.msg,
+      // group: ........
+    }
+    let _newMsg = new MessageModel(newMsg);
+    _newMsg.save();
+    core.logger.info('发送消息成功');
+  });
+  // 断开连接时
+  socket.on('disconnect', async () => {
+    let index = users.indexOf(socket.nickname);
+    if (index > -1) {
+      users.splice(index, 1); // 删除用户信息
+      usersInfo.splice(index, 1);
+
+      io.emit('system', {
+        name: socket.nickname,
+        status: '离开'
+      });
+      await UserModel.update({name: socket.nickname}, {
+        $set: {
+          online: false
+        }
+      })
+      io.emit('displayUser', usersInfo);   // 重新渲染在线人员
+      core.logger.info('a user left.')
+    }
+  })
+
+})
+
+
 app.use(session({
   resave: true,
   cookie: {
@@ -62,6 +184,9 @@ app.use(session({
   saveUninitialized: true,
   secret: config.sessionSecret || 'chat',
 }))
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
@@ -80,12 +205,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.set('port', process.env.PORT || config.port || 4000);
-let server = app.listen(app.get('port'), function() {
-  core.logger.info('网站启动端口： '+ server.address().port);
-  core.logger.info('环境变量： '+ config.env);
-  core.logger.info('mongodb url' + config.mongodb.uri);
-  core.logger.info('redis url' + config.redis.host+ ":" + config.redis.port);
-})
 
 module.exports = app;
